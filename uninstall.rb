41ff7e8e8ab5b738921839dcf891beb9bf5e7950
Gareth Rees <gareth@mysociety.org>
2014-08-27 12:21:16 +0100

    Add User#id_number attribute

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,12 @@
 # Uninstall hook code here
+
+def column_exists?(table, column)
+  # TODO: ActiveRecord 3 includes "column_exists?" method on `connection`
+  ActiveRecord::Base.connection.columns(table.to_sym).collect{|c| c.name.to_sym}.include?(column)
+end
+
+# Remove the id_number field to the User model
+if column_exists?(:users, :id_number)
+  require File.expand_path '../db/migrate/derechoapreguntar_theme_add_id_number_to_user', __FILE__
+  DerechoaPreguntarThemeAddIdNumberToUser.down
+end
