ac3c1f77796178fceace89b72b3048081abf475f
Gareth Rees <gareth@mysociety.org>
2014-09-08 12:56:11 +0100

    Add basic GeneralLaw

--- post_install.rb
+++ post_install.rb
@@ -1,9 +1,14 @@
 # This file is executed in the Rails evironment by the `rails-post-deploy`
 # script
 
+def table_exists?(table)
+  ActiveRecord::Base.connection.table_exists?(table)
+end
+
 def column_exists?(table, column)
-  # TODO: ActiveRecord 3 includes "column_exists?" method on `connection`
-  ActiveRecord::Base.connection.columns(table.to_sym).collect{|c| c.name.to_sym}.include?(column)
+  if table_exists?(table)
+    ActiveRecord::Base.connection.column_exists?(table, column)
+  end
 end
 
 # Add the identity_card_number field to the User model
@@ -11,4 +16,10 @@ unless column_exists?(:users, :identity_card_number)
   migration_file_path = '../db/migrate/derechoapreguntar_theme_add_identity_card_number_to_user'
   require File.expand_path migration_file_path, __FILE__
   DerechoaPreguntarThemeAddIdentityCardNumberToUser.up
+end
+
+unless table_exists?(:general_laws)
+  migration_file_path = '../db/migrate/derechoapreguntar_theme_create_general_laws'
+  require File.expand_path migration_file_path, __FILE__
+  DerechoaPreguntarThemeCreateGeneralLaws.up
 end
