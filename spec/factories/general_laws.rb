2742b808bbd643b51c7711f1bf3dc952799153b0
Gareth Rees <gareth@mysociety.org>
2014-09-11 13:33:40 +0100

    Use Factories in specs

---
+++ spec/factories/general_laws.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :general_law do
+    date_of_birth Date.yesterday
+    marital_status 'single'
+    occupation 'programmer'
+    domicile 'Nicaragua'
+  end
+
+end
