42896f19e358b1e8128554d7f5348d82a73d0931
Gareth Rees <gareth@mysociety.org>
2014-09-12 10:42:37 +0100

    Add form for editing GeneralLaw

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,5 +1,14 @@
 # Here you can override or add to the pages in the core website
 Rails.application.routes.draw do
+
+  match '/profile/general_law/edit' => 'general_laws#edit',
+        :as => :edit_general_law,
+        :via => :get
+
+  match '/profile/general_law' => 'general_laws#update',
+        :as => :general_law,
+        :via => :put
+
   match '/profile/identity_card_number/edit' => 'user_identity_card_number#edit',
         :as => :user_edit_identity_card_number,
         :via => :get
