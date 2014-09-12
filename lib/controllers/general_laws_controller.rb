976e90eae2db2692d1e438e144f27d5eb1900780
Gareth Rees <gareth@mysociety.org>
2014-09-12 10:42:37 +0100

    Add form for editing GeneralLaw

---
+++ lib/controllers/general_laws_controller.rb
@@ -0,0 +1,32 @@
+class GeneralLawsController < ApplicationController
+  before_filter :authenticate
+
+  def edit
+    @general_law = @user.general_law
+  end
+
+  def update
+    @general_law = @user.general_law
+
+    if @general_law.update_attributes(params[:general_law])
+      redirect_to show_user_profile_path(:url_name => @user.url_name),
+                  :notice => _('Your General Law information was successfully updated')
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+  def authenticate
+    unless authenticated?(
+      :web => _("To change your email address used on {{site_name}}", :site_name => site_name),
+      :email => _("Then you can change your email address used on {{site_name}}", :site_name => site_name),
+      :email_subject => _("Change your email address used on {{site_name}}",:site_name => site_name)
+    )
+      # "authenticated?" has done the redirect to signin page for us
+      return
+    end
+  end
+
+end
---
+++ spec/controllers/general_laws_controller_spec.rb
@@ -0,0 +1,89 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe GeneralLawsController do
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    session[:user_id] = @user.id
+  end
+
+  describe :edit do
+
+    it 'requires login' do
+      session[:user_id] = nil
+
+      get :edit
+
+      post_redirect = PostRedirect.get_last_post_redirect
+      expect(response).to redirect_to(
+        :controller => 'user',
+        :action => 'signin',
+        :token => post_redirect.token)
+    end
+
+    it 'finds the logged in user' do
+      get :edit
+      expect(assigns[:user]).to eql(@user)
+    end
+
+    it 'finds the general law belonging to the user' do
+      get :edit
+      expect(assigns(:general_law)).to eq(@user.general_law)
+    end
+
+    it 'renders the edit template' do
+      get :edit
+      expect(response).to render_template('edit')
+    end
+
+  end
+
+  describe :update do
+
+    let(:general_law_params) { @user.general_law.serializable_hash }
+
+    it 'requires login' do
+      session[:user_id] = nil
+
+      put :update, { :general_law => nil }
+
+      post_redirect = PostRedirect.get_last_post_redirect
+      expect(response).to redirect_to(
+        :controller => 'user',
+        :action => 'signin',
+        :token => post_redirect.token)
+    end
+
+    it 'finds the logged in user' do
+      put :update, { :general_law => general_law_params }
+      expect(assigns[:user]).to eql(@user)
+    end
+
+    it 'assigns the post paramters' do
+      post :update, :general_law => general_law_params
+      expect(assigns(:general_law)).to be_an_instance_of(GeneralLaw)
+      expect(assigns(:general_law).domicile).to eq(general_law_params['domicile'])
+    end
+
+    it 'redirects to index on save' do
+      GeneralLaw.any_instance.stub(:update_attributes).and_return(true)
+      post :update, :general_law => general_law_params
+      path = show_user_profile_path(:url_name => @user.url_name)
+      expect(response).to redirect_to(path)
+    end
+
+    it 'contains flash message on save' do
+      GeneralLaw.any_instance.stub(:update_attributes).and_return(true)
+      post :update, :general_law => general_law_params
+      expect(flash[:notice]).to_not be_blank
+    end
+
+    it 'renders edit on failure' do
+      GeneralLaw.any_instance.stub(:update_attributes).and_return(false)
+      post :update, :general_law => general_law_params
+      expect(response).to render_template('edit')
+    end
+
+  end
+
+end
